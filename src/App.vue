5c2e9f317a6b8750962e3b55d4969d5f5e23523b
Ilya <iovodovi@yandex.ru>
2025-03-24 12:15:23 +0300

    Refactor app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import UserDictionary from './components/UserDictionary/UserDictionary.vue';
 import { UserDictionaryOptionMovementMode, UserDictionaryOptionDeliveryStatus } from './components/UserDictionary/UserDictionaryOptions'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-main>
+      <v-container class="fill-height">
+        <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+          <v-img class="mb-4" height="150" src="../src/assets/logo.svg" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+          <div class="text-center">
+            <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-  <main>
-    <UserDictionary title="Статус поставки" sub-title="Статус" :options="UserDictionaryOptionDeliveryStatus" />
-    <UserDictionary title="Режим перемещения" sub-title="Режим" :options="UserDictionaryOptionMovementMode" />
-  </main>
+            <h1 class="text-h2 font-weight-bold">{{ "User dictionary app" }}</h1>
+          </div>
+
+          <div class="py-4" />
+
+          <v-row>
+            <v-col cols="6">
+              <UserDictionary title="Статус поставки" sub-title="Статус"
+                :options="UserDictionaryOptionDeliveryStatus" />
+            </v-col>
+
+            <v-col cols="6">
+              <UserDictionary title="Режим перемещения" sub-title="Режим" :options="UserDictionaryOptionMovementMode" />
+            </v-col>
+          </v-row>
+        </v-responsive>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
